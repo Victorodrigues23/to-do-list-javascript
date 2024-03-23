1a6d350dde02672879a885cd2d713c76dd273e92
victor rodrigues <victorodrigues2312@gmail.com>
2024-03-23 12:41:32 -0300

    incio do javascript

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
-function newTask() {}
+const localStorageKey = "to-do-list-gn";
+
+function newTask() {
+  let input = document.querySelector("#input-task");
+
+  if (!input.value) {
+    alert("digite algo para inserir em sua Lista !");
+  } else {
+    let values = JSON.parse(localStorage.getItem(localStorageKey) || "[]");
+    values.push({
+      name: input.value,
+    });
+
+    localStorage.setItem(localStorageKey, JSON.stringify(values));
+    showValues();
+  }
+}
+
+function showValues() {
+  let values = JSON.parse(localStorage.getItem(localStorageKey) || "[]");
+  let list = document.querySelector("#to-do-list");
+  list.innerHTML = "";
+  for (let i = 0; i < values.length; i++) {
+    list.innerHTML += `<li>${values[i]["name"]}<button id ='btn-ok' onclick = 'removeItem("${values[i]["name"]}")'>ok</button></li>`;
+  }
+}
+
+function removeItem(data) {
+  console.log(data);
+}
+showValues();
